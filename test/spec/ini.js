f204bbc4676e2fedfefdd221a2a2f8737e245895
aleksnick <aleksnick@list.ru>
2013-11-07 11:23:11 +0400

    покрытие тестами парсера ini

---
+++ test/spec/ini.js
@@ -0,0 +1,104 @@
+
+var asser, ini;
+
+process.env.APP_DIR = '../..';
+
+assert = require('chai').assert;
+
+ini = require(process.env.APP_DIR + '/lib/controllers/ini');
+
+describe('Ini:', function() {
+
+  describe('Parsing ini:', function() {
+
+    it('Simple string', function() {
+      var 
+        text =  "[insertsql] \r\n" +
+                "\r\n" +
+                "select D$UUID from pr_add_device(:vid,:building_d$uuid,:APARTMENT)\r\n",
+
+        json = {
+            "insertsql" : "select D$UUID from pr_add_device(:vid,:building_d$uuid,:APARTMENT)"
+        };
+
+      assert.deepEqual(json, ini.parse(text));
+    });
+
+    it('Simple string with "="', function() {
+      var 
+        text =  "[deletesql_selected]\r\n" +
+                "update VW_DEVICE set status=iif(status=1,0,1) where d$uuid in (:selected_ids: )\r\n",
+
+        json = {
+            "deletesql_selected" : "update VW_DEVICE set status=iif(status=1,0,1) where d$uuid in (:selected_ids: )"
+        };
+
+      assert.deepEqual(json, ini.parse(text));
+    });
+
+    it('One string and empty part', function() {
+      var
+        text =  "[refreshsql]\r\n" +
+                "select * from VW_DEVICE where d$uuid=:d$uuid\r\n" +
+                "\r\n" +
+                "[selectsql]\r\n" +
+                "\r\n",
+
+        json = {
+            "refreshsql" : "select * from VW_DEVICE where d$uuid=:d$uuid",
+            "selectsql": {}
+        };
+
+      assert.deepEqual(json, ini.parse(text));
+    });
+
+    it('Key-value', function() {
+      var
+        text =  "[cfSelect]\r\n" +
+                "selectfieldexpression=SELECTCAPTION\r\n" +
+                "AllwaysPartial=1\r\n" +
+                "\r\n" +
+                "[form_show]\r\n" +
+                "position=0\r\n" +
+                "left=1\r\n" +
+                "\r\n",
+
+        json = {
+            "cfSelect": {
+              "selectfieldexpression": "SELECTCAPTION",
+              "AllwaysPartial": "1"
+            },
+            "form_show": {
+              "position": "0",
+              "left": "1"
+            }
+        };
+
+      assert.deepEqual(json, ini.parse(text));
+    });
+
+    it('Key-value and strings', function() {
+      var
+        text =  "[cfSelect]\r\n" +
+                "selectfieldexpression=SELECTCAPTION\r\n" +
+                "AllwaysPartial=1\r\n" +
+                "\r\n" +
+                "[selectsql]\r\n" +
+                "select * from VW_DEVICE where status=0 order by street, sortedcaptionb, sortedcaptiona,d$uuid\r\n" +
+                "\r\n",
+
+        json = {
+            "cfSelect": {
+              "selectfieldexpression": "SELECTCAPTION",
+              "AllwaysPartial": "1"
+            },
+            "selectsql": "select * from VW_DEVICE where status=0 order by street, sortedcaptionb, sortedcaptiona,d$uuid"
+        };
+
+      assert.deepEqual(json, ini.parse(text));
+    });
+
+
+  });
+
+});
